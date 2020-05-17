49a017293b368a383451d20bf67b045add952bcb
Luke Turner <github@luketurner.org>
2020-05-17 16:58:59 -0700

    Remove unused --static-ip option

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,8 +3,6 @@ set -e
 
 if [[ "$1" == "--user" ]]; then
   scripts/initial_login
-elif [[ "$1" == "--static-ip" ]]; then
-  ansible-playbook -i inventory.ini playbooks/pinode/static_ip.yml
 else
   ansible-playbook -i inventory.ini playbooks/pinode/main.yml
   ansible-playbook -i inventory.ini playbooks/pinode/k3s.yml
